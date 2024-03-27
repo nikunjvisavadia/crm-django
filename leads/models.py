76c5f601ff227d0de404b81c65a9eae0b595a65c
Nikunj Visavadia <nikunjvisavadia55@gmail.com>
2024-03-27 23:54:38 +0530

    Agent In Lead Model

--- leads/models.py
+++ leads/models.py
@@ -3,4 +3,18 @@ from django.db import models
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    age =models.IntegerField(default=0)
+    age = models.IntegerField(default=0)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey("Category", related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    profile_picture = models.ImageField(null=True, blank=True, upload_to="profile_pictures/")
+    converted_date = models.DateTimeField(null=True, blank=True)
+    
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
