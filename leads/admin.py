1856b2ede04ee4955ddb0a8d98fd9ab545466103
Nikunj Visavadia <nikunjvisavadia55@gmail.com>
2024-03-28 23:44:21 +0530

    Db and Admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Agent,User,Lead
+
+admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(Agent)
--- leads/models.py
+++ leads/models.py
@@ -1,20 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
-    category = models.ForeignKey("Category", related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
-    description = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
-    phone_number = models.CharField(max_length=20)
-    email = models.EmailField()
-    profile_picture = models.ImageField(null=True, blank=True, upload_to="profile_pictures/")
-    converted_date = models.DateTimeField(null=True, blank=True)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
     
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"    
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.email
