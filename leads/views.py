1c35f471d8066bb251363fdf0e02ed6b238c1226
Nikunj Visavadia <nikunjvisavadia55@gmail.com>
2024-04-01 17:07:08 +0530

    Added Static And Mail

--- leads/views.py
+++ leads/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import *
@@ -43,6 +44,14 @@ class LeadCreateView(CreateView):
     
     def get_success_url(self):
         return reverse("leads:lead-list")
+    
+    def form_valid(self,form):
+        #TODO send mail
+        send_mail(
+            subject="A lead has been Created", message="Go to the site to see the new lead", from_email="test@test.com", recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView, self).form_valid(form)    
+
 
 def lead_create(request):
     print(request.POST)
