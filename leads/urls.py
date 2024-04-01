f916ebfea6fa856dacba5b3a27e44aa010e6ec7a
Nikunj Visavadia <nikunjvisavadia55@gmail.com>
2024-04-01 07:56:03 +0530

    Generic Class Based View for CRUD operations

--- leads/urls.py
+++ leads/urls.py
@@ -4,9 +4,9 @@ from .views import *
 app_name="leads"
 
 urlpatterns = [
-    path('', lead_list, name='lead-list'),
-    path('create/', lead_create, name='lead-create'),
-    path('<int:pk>', lead_detail, name='lead-detail'),
-    path('<int:pk>/update', lead_update, name='lead-update'),
-    path('<int:pk>/delete', lead_delete, name='lead-delete'),
+    path('', LeadListView.as_view(), name='lead-list'),
+    path('create/', LeadCreateView.as_view(), name='lead-create'),
+    path('<int:pk>', LeadDetailView.as_view(), name='lead-detail'),
+    path('<int:pk>/update', LeadUpdateView.as_view(), name='lead-update'),
+    path('<int:pk>/delete', LeadDeleteView.as_view(), name='lead-delete'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import *
 from .forms import *
 
+class LandingPageView(TemplateView):
+    template_name="landing.html"
 
 def landing_page(request):
     return render(request, "landing.html")
+
+
+class LeadListView(ListView):
+    template_name='leads/lead_list.html'
+    queryset=Lead.objects.all() # here in context lead will be change to object_list
+    context_object_name='leads' #again contect object name changes to leads
+
 def lead_list(request):
     leads=Lead.objects.all()
     context={
@@ -14,12 +24,38 @@ def lead_list(request):
     return render(request,"leads/lead_list.html", context)
 
 
+class LeadDetailView(DetailView):
+    template_name='leads/lead_detail.html'
+    queryset=Lead.objects.all() # here in context lead will be change to object_list
+    context_object_name='lead' #again contect object name changes to leads
+
 def lead_detail(request, pk):
     lead=Lead.objects.get(id=pk)
     context={
         "lead":lead
     }
     return render(request, "leads/lead_detail.html",context)
+
+
+class LeadCreateView(CreateView):
+    template_name='leads/lead_create.html'
+    form_class= LeadModelForm
+    
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+def lead_create(request):
+    print(request.POST)
+    form = LeadModelForm()
+    if request.method == "POST":
+        form=LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context={
+        "form":form,
+    }
+    return render(request, "leads/lead_create.html", context)
 
 
 # def lead_create(request):
@@ -49,19 +85,13 @@ def lead_detail(request, pk):
 #     return render(request, "leads/lead_create.html", context)
 
 
-def lead_create(request):
-    print(request.POST)
-    form = LeadModelForm()
-    if request.method == "POST":
-        form=LeadModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("/leads")
-    context={
-        "form":form,
-    }
-    return render(request, "leads/lead_create.html", context)
-
+class LeadUpdateView(UpdateView):
+    template_name='leads/lead_update.html'
+    form_class= LeadModelForm
+    queryset=Lead.objects.all() # here in context lead will be change to object_list
+    context_object_name='lead' #again contect object name changes to leads
+    def get_success_url(self):
+        return reverse("leads:lead-list")
 
 def lead_update(request,pk):
     lead=Lead.objects.get(id=pk)
@@ -102,6 +132,12 @@ def lead_update(request,pk):
 #     return render(request, "leads/lead_update.html", context)
 
 
+class LeadDeleteView(DeleteView):
+    template_name='leads/lead_delete.html'
+    queryset=Lead.objects.all() # here in context lead will be change to object_list
+    
+    def get_success_url(self):
+        return reverse("leads:lead-list")
 
 def lead_delete(request,pk):
     lead= Lead.objects.get(id=pk)
